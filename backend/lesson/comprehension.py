1804904a93c9d649f710a4c35f6dd66d0d227e1b
吉村直人 <ahowateto123@gmail.com>
2023-10-03 23:45:38 +0900

    辞書内包表記

---
+++ backend/lesson/comprehension.py
@@ -0,0 +1,45 @@
+# リスト内包表記
+t = (1, 2, 3, 4, 5)
+t2 = (5, 6, 7, 8, 9, 10)
+
+r = []
+for i in t:
+    r.append(i)
+
+print(r)
+
+r = [i for i in t if (i % 2 == 0)]
+
+print(r)
+
+r = []
+for i in t:
+    for j in t2:
+        r.append(i * j)
+
+print(r)
+
+# わかりにくいから2階層以上の場合は非推奨
+r = [(i * j) for i in t for j in t2]
+
+print(r)
+
+
+
+# 辞書内包表記
+w = ['mon', 'tue', 'wed']
+f = ['coffee', 'milk', 'water']
+
+d = {}
+for k, v in zip(w, f):
+    d[k] = v
+
+print(d)
+
+d = {k: v for k, v in zip(w, f)}
+print(d)
+
+
+
+# 集合内包表記
+
