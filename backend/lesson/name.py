0c92cb356ba13e37426a842adcd3b6ea14bacb25
吉村直人 <ahowateto123@gmail.com>
2023-10-12 16:13:51 +0000

    __name__の使い方

---
+++ backend/lesson/name.py
@@ -0,0 +1,8 @@
+import config
+import packages.talk.animal
+
+def main():
+    packages.talk.animal.sing()
+
+if __name__ == '__main__':
+    main()
--- backend/lesson/packages/talk/animal.py
+++ backend/lesson/packages/talk/animal.py
@@ -5,3 +5,7 @@ def sing():
 
 def cry():
     return(utils.say_twice('えん'))
+
+if __name__ == '__main__':
+    print(sing())
+    print('animal:', __name__)
