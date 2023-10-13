0afec2464eb68200babf27d59ac258a2f2490f57
吉村直人 <ahowateto123@gmail.com>
2023-10-13 22:47:47 +0900

    ファイルの書き込み読み込みモード

---
+++ backend/lesson/file/read_write.py
@@ -0,0 +1,17 @@
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+
+# with open('test.txt', 'w+') as f:
+#     f.write(s)
+#     f.seek(0)
+#     print(f.read())
+
+with open('test.txt', 'r+') as f:
+    print(f.read())
+    f.seek(0)
+    f.write(s)
+
