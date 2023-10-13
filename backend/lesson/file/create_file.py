eaadf16cfcf437e8553e4a94d4bebc4119abd51c
吉村直人 <ahowateto123@gmail.com>
2023-10-13 22:31:47 +0900

    ファイル読み込み

--- backend/lesson/file/create_file.py
+++ backend/lesson/file/create_file.py
@@ -1,2 +1,24 @@
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+
 with open('test.txt', 'a') as f:
-    f.write('Test\n')
+    f.write(s)
+
+with open('test.txt', 'r') as f:
+    while True:
+        line = f.readline()
+        print(line, end='')
+        if not line:
+            break
+
+with open('test.txt', 'r') as f:
+    while True:
+        chunk = 2
+        line = f.read(chunk)
+        print(line)
+        if not line:
+            break
