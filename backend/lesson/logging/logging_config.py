a41578a8f1ed76d3034239a4321583d8e1b37d45
吉村直人 <ahowateto123@gmail.com>
2023-10-17 05:30:38 +0900

    ロギング コンフィグ

---
+++ backend/lesson/logging/logging_config.py
@@ -0,0 +1,33 @@
+import logging.config
+
+logging.config.dictConfig({
+    'version': 1,
+    'formatters': {
+        'sampleFormatter': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
+    'handlers': {
+        'sampleHandlers': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'sampleFormatter',
+            'level': logging.DEBUG
+        }
+    },
+    'root': {
+        'handlers': ['sampleHandlers'],
+        'level': logging.WARNING,
+    },
+    'loggers': {
+        'simpleExample': {
+            'handlers': ['sampleHandlers'],
+            'level': logging.DEBUG,
+            'propagate': 0,
+        }
+    }
+})
+
+logger = logging.getLogger('simpleExample')
+
+logger.warning('warning')
+logger.error('error')
