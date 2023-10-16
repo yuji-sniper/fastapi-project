901c89a81afe53d5b01dca5faecfc9a1aa0696f4
吉村直人 <ahowateto123@gmail.com>
2023-10-17 05:08:31 +0900

    ログ ハンドラー

--- backend/lesson/logging/log_handler.py
+++ backend/lesson/logging/log_handler.py
@@ -4,6 +4,10 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+handler = logging.FileHandler('log_handler.log')
+logger.addHandler(handler)
+
+
 def do_something():
     logger.info('info')
     logger.debug('debug')
--- backend/lesson/logging/logger_lesson.py
+++ backend/lesson/logging/logger_lesson.py
@@ -1,12 +1,14 @@
 import logging
+import log_handler
 import log_test
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.debug('debug')
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+# logger.debug('debug')
 
+log_handler.do_something()
 log_test.do_something()
